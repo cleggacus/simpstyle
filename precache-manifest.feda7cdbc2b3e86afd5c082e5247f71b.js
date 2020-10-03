9c3fa09ac262c27ce2e9b215d4c79e5b9c4b1577
Liam Clegg <cleggacus@gmail.com>
2020-10-03 23:15:17 +0100

    Updates

---
+++ precache-manifest.feda7cdbc2b3e86afd5c082e5247f71b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038e5633b0acb3e375951d8e56cf8863",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e2deec6ec35b12e861e",
+    "url": "./static/css/main.c6dc3197.chunk.css"
+  },
+  {
+    "revision": "9f315d5b1de562c74d95",
+    "url": "./static/js/2.2e121434.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2e121434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2deec6ec35b12e861e",
+    "url": "./static/js/main.67ad5826.chunk.js"
+  },
+  {
+    "revision": "87a2786e626a08bb4425",
+    "url": "./static/js/runtime-main.761b5d4a.js"
+  }
+]);
